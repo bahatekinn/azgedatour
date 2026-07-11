b40465587d794d2368743b63337fd851cab716c2
bahatekinn <bahatekinnn@gmail.com>
2026-07-11 20:24:49 +0300

    Oda sistemi eklendi

--- server.js
+++ server.js
@@ -3,14 +3,12 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-// YENİ: public klasörünü dışarıya açarak avatarların tarayıcıda okunmasını sağlıyoruz
 app.use('/public', express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-// 32 karelik haritada mülk olan yerlerin ID listesi
 const mulkKareleri = [1,2,4,5,6,7,9,10,11,13,14,15,17,18,19,21,22,23,25,26,27,29,30,31];
 let festivaller = [];
 while(festivaller.length < 3) {
@@ -18,16 +16,24 @@ while(festivaller.length < 3) {
     if(!festivaller.includes(rastgele)) festivaller.push(rastgele);
 }
 
+// Herkesi aynı odaya sokmak için oda ismi
+const ANA_ODA = "azgeda-lobi";
+
 io.on('connection', (socket) => {
     console.log('Bir oyuncu bağlandı! ID:', socket.id);
+    
+    // Bağlanan herkesi otomatik olarak ana odaya dahil et
+    socket.join(ANA_ODA);
 
-    // Bağlanan oyuncuya anında festivalleri bildir
+    // Bağlanan oyuncuya festivalleri gönder
     socket.emit('festival-bilgisi', festivaller);
 
     socket.on('zar-at', () => {
         const zar1 = Math.floor(Math.random() * 6) + 1;
         const zar2 = Math.floor(Math.random() * 6) + 1;
-        io.emit('zar-sonucu', { 
+        
+        // io.to(ANA_ODA) diyerek sadece bu odadaki kişilere sonucu gönderiyoruz
+        io.to(ANA_ODA).emit('zar-sonucu', { 
             oyuncuId: socket.id, 
             deger: zar1 + zar2, 
             zar1: zar1, 
@@ -37,15 +43,16 @@ io.on('connection', (socket) => {
     });
 
     socket.on('mulk-islem', (data) => {
-        socket.broadcast.emit('mulk-islem-bilgisi', data);
+        // Sadece odadakilere gönder
+        socket.to(ANA_ODA).emit('mulk-islem-bilgisi', data);
     });
 
     socket.on('uctu', (data) => {
-        socket.broadcast.emit('uctu-bilgisi', data);
+        socket.to(ANA_ODA).emit('uctu-bilgisi', data);
     });
 
     socket.on('para-guncelle', (data) => {
-        socket.broadcast.emit('para-guncelle-bilgisi', data);
+        socket.to(ANA_ODA).emit('para-guncelle-bilgisi', data);
     });
 });
 
