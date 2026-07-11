676501282fedda5c4389d9e7c5d5c7db2cc9fd43
bahatekinn <bahatekinnn@gmail.com>
2026-07-11 23:36:43 +0300

    Buraya yaptığın değişikliğin kısa bir notunu yaz

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-// Hangi odada hangi oyuncular var onu tutalım
+// Hangi odada hangi oyuncular var ve pozisyonları ne?
 let odadakiOyuncular = {}; 
 
 const mulkKareleri = [1,2,4,5,6,7,9,10,11,13,14,15,17,18,19,21,22,23,25,26,27,29,30,31];
@@ -22,32 +22,37 @@ while(festivaller.length < 3) {
 io.on('connection', (socket) => {
     console.log('Bir oyuncu bağlandı! ID:', socket.id);
     
-    // ODA KODU VE OYUNCU BİLGİSİ
     socket.on('oda-degis', (yeniOda) => {
         socket.join(yeniOda);
         socket.currentRoom = yeniOda; 
         socket.emit('festival-bilgisi', festivaller);
     });
 
-    // YENİ OYUNCU KATILDIĞINDA (Bunu index.html'den çağırman lazım)
+    // YENİ OYUNCU KATILDIĞINDA
     socket.on('yeni-oyuncu-katildi', (data) => {
         if (!odadakiOyuncular[socket.currentRoom]) odadakiOyuncular[socket.currentRoom] = [];
         
-        let oyuncuBilgisi = { id: socket.id, nick: data.nick, avatar: data.avatar };
+        // Oyuncuya başlangıç pozisyonu (0) ekliyoruz
+        let oyuncuBilgisi = { id: socket.id, nick: data.nick, avatar: data.avatar, pos: 0 };
         odadakiOyuncular[socket.currentRoom].push(oyuncuBilgisi);
         
-        // Diğerlerine yeni oyuncuyu haber ver
         socket.to(socket.currentRoom).emit('oyuncu-listesini-guncelle', oyuncuBilgisi);
-        
-        // Yeni gelene de odadakileri gönder
         socket.emit('mevcut-oyuncular', odadakiOyuncular[socket.currentRoom]);
+    });
+
+    // OYUNCU HAREKET ETTİĞİNDE POZİSYONU GÜNCELLE
+    socket.on('piyon-hareket-etti', (data) => {
+        if (odadakiOyuncular[socket.currentRoom]) {
+            let oyuncu = odadakiOyuncular[socket.currentRoom].find(o => o.id === socket.id);
+            if (oyuncu) oyuncu.pos = data.yeniPos;
+            // Herkese güncel listeyi gönder ki herkes piyonları doğru yere çizsin
+            io.to(socket.currentRoom).emit('tum-oyuncular-guncellendi', odadakiOyuncular[socket.currentRoom]);
+        }
     });
 
     // SOHBET
     socket.on('mesaj-yolla', (data) => {
-        if (socket.currentRoom) {
-            io.to(socket.currentRoom).emit('mesaj-al', data);
-        }
+        if (socket.currentRoom) io.to(socket.currentRoom).emit('mesaj-al', data);
     });
 
     // ZAR
@@ -55,7 +60,6 @@ io.on('connection', (socket) => {
         if (socket.currentRoom) {
             const zar1 = Math.floor(Math.random() * 6) + 1;
             const zar2 = Math.floor(Math.random() * 6) + 1;
-            
             io.to(socket.currentRoom).emit('zar-sonucu', { 
                 oyuncuId: socket.id, 
                 deger: zar1 + zar2, 
@@ -66,15 +70,14 @@ io.on('connection', (socket) => {
         }
     });
 
-    // MÜLK, UÇUŞ VE PARA (Diğerleri aynı)
+    // MÜLK, UÇUŞ VE PARA
     socket.on('mulk-islem', (data) => { socket.to(socket.currentRoom).emit('mulk-islem-bilgisi', data); });
     socket.on('uctu', (data) => { socket.to(socket.currentRoom).emit('uctu-bilgisi', data); });
     socket.on('para-guncelle', (data) => { socket.to(socket.currentRoom).emit('para-guncelle-bilgisi', data); });
 
-    // Çıkış yapınca listeden sil
     socket.on('disconnect', () => {
         console.log('Oyuncu ayrıldı:', socket.id);
-        // İstersen burada odadakiOyuncular listesinden de silebilirsin
+        // İstersen burada odadakiOyuncular listesinden silme işlemini ekleyebilirsin
     });
 });
 
